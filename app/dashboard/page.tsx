20ee357d20caeb340f770462c8abc4311131141b
sahineFaniria <faniriasahine@gmail.com>
2024-05-11 22:06:10 +0300

    chapiter 9

--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page(){
-    return (
-        <div className="">
-            <h1>Dashboard Page</h1>
-            <button className=" bg-sky-700 px-2 py-1 text-white rounded text-sm" >Valider</button>
-        </div>
-    )
-}
