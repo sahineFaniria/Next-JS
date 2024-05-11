4b3b0bc54af65f266f9f40d6882f8fdb530bd5a4
sahineFaniria <faniriasahine@gmail.com>
2024-05-11 10:21:41 +0300

    dashboard button

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
 export default function Page(){
-    return <p>Dashboard page</p>
+    return (
+        <div className="">
+            <h1>Dashboard Page</h1>
+            <button className=" bg-sky-700 px-2 py-1 text-white rounded text-sm" >Valider</button>
+        </div>
+    )
 }
